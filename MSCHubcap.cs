8bff88645994df4ab632ccba1fbeb07b21783e17
Kacper Gibas <kacperbogangibas@gmail.com>
2025-01-23 10:21:33 +0100

    0.1 Release

---
+++ MSCHubcap.cs
@@ -0,0 +1,36 @@
+﻿using MSCLoader;
+using UnityEngine;
+
+namespace MSCHubcap
+{
+    public class Hubcap : Mod
+    {
+        public override string ID => "MSCHubcap";
+        public override string Name => "Custom Hubcap";
+        public override string Author => "exyxz";
+        public override string Version => "0.1";
+        public override string Description => "Replaces stock hubcaps with custom one";
+        public override bool UseAssetsFolder => true;
+
+        public override void ModSetup()
+        {
+            SetupFunction(Setup.OnLoad, Mod_OnLoad);
+        }
+
+        private void Mod_OnLoad()
+        {
+            AssetBundle bundle = LoadAssets.LoadBundle(this, "hubcap.unity3d");
+            GameObject mGO = Object.Instantiate(bundle.LoadAsset<GameObject>("Hubcap.prefab"));
+            bundle.Unload(false);
+            GameObject[] hubcaps = GameObject.FindGameObjectsWithTag("PART");
+            foreach (GameObject hubcap in hubcaps)
+            {
+                if (hubcap.name == "hubcap(Clone)")
+                {
+                    hubcap.GetComponent<MeshFilter>().mesh = mGO.GetComponent<MeshFilter>().mesh;
+                    hubcap.GetComponent<MeshRenderer>().material = mGO.GetComponent<MeshRenderer>().material;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# MSCHubcap
+
+MSCHubcap is an modification for My Summer Car that allows to replace stock hubcaps with custom model.
+
+For more information and/or prebuilt binaries, check out at [NexusMods](https://www.nexusmods.com/mysummercar/mods/7497).
